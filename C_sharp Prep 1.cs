cb19a8827cafcbebb69f419f3a82b407e8329a7a
Borrel28 <113734040+Borrel28@users.noreply.github.com>
2023-04-22 21:46:06 +0000

    C_sharp Prep 1 and 2
    week1

---
+++ C_sharp Prep 1.cs
@@ -0,0 +1,17 @@
+class Program{
+    static void Main(string[] args)
+    {
+        // the first Name
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
+        // the last Name
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+        // the print or output part      
+        Console.WriteLine($"Your full name are: {first}, {last}!");
+        
+    }
+}
+
+
+
